50d663b91418ceae7756ca3c8a7650cc01a286cd
yoojen <eugeneemma7@gmail.com>
2024-01-14 23:56:10 +0200

    fixed anotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,9 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """return filtered input message"""
     for field in fields:
         message = re.sub(rf'(?<={field}=)[^{separator}]+',
