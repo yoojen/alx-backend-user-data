4b55e8688b77f24899856a8aabaf2f9d699a39eb
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-14 23:53:06 +0200

    function anotatated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """replace in a string using regex"""
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message: str, separator):
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
     """return filtered input message"""
     for field in fields:
         message = re.sub(rf'(?<={field}=)[^{separator}]+',
