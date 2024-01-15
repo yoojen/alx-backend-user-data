f0dacaf3da2f5a28decf7f94240e9803279f92c3
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-15 11:10:22 +0200

    inserted PII_FIELDS

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,8 @@ import re
 from typing import List
 import logging
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
