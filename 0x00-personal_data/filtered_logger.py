5598f9dc288d297b23d23524e58b54f4875d0a55
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-15 22:32:17 +0200

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -50,12 +50,12 @@ def get_logger() -> logging.Logger:
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """connect to myql db"""
+    """connect to myql db using environment variables"""
     db_config = {
-        "host": os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
         "user": os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
         "password": os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
-        "database": os.environ.get("PERSONAL_DATA_DB_NAME", "my_db")
+        "host": os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        "database": os.environ.get("PERSONAL_DATA_DB_NAME")
     }
     conn = connector.connection.MySQLConnection(**db_config)
     return conn
