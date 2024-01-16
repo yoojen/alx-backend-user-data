94197700d02783a2b9b1d82e0caba46b4ff6df61
Eugene <eugenemutuyimana15@gmail.com>
2024-01-16 15:34:29 +0200

    implemented require authorization

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,19 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """check if user is authorized"""
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        filtered_paths = excluded_paths[:]
+        if path:
+            if path[-1] != '/':
+                path = path + '/'
+            for excluded in excluded_paths:
+                if excluded[-1] != '/':
+                    filtered_paths.append(excluded + '/')
+            if path not in filtered_paths:
+                return True
+            else:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """check for authorized header"""
