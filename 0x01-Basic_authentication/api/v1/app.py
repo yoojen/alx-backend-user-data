69f9f2ff2be256872d66ff10a68109ea212e19ce
Eugene <eugenemutuyimana15@gmail.com>
2024-01-16 16:38:47 +0200

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -18,7 +18,7 @@ AUTH_TYPE = getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif AUTH_TYPE == 'basic_auth':
+elif AUTH_TYPE == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
