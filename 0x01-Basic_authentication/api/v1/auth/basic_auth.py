5655e7f3459c3f98d29ceab485f100e6d4571bde
Eugene <eugenemutuyimana15@gmail.com>
2024-01-16 22:48:43 +0200

    decode base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -12,7 +12,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
-        """manages the API authentication"""
+        """extract base64 authentication header"""
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """return decoded authorization header"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            convertable_bytes = base64_authorization_header.encode('utf-8')
+            converted_bytes = b64decode(convertable_bytes)
+            decoded_string = converted_bytes.decode('utf-8')
+        except BaseException:
+            return None
+        return decoded_string
