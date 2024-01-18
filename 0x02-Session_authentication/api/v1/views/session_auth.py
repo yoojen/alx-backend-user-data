d1d4b4ce6636a2a74506f6c1bd55e7d86da5b844
Eugene <eugenemutuyimana15@gmail.com>
2024-01-19 00:24:57 +0200

    view that handles login

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" view that deals with authantication
+"""
+
+from api.v1.views import app_views
+from flask import request, jsonify
+from models.user import User
+from os import getenv
+
+
+@app_views.route('POST /auth_session/login', methods=['POST'],
+                 strict_slashes=False)
+def handles_session():
+    """view that handles all routes for the Session authentication."""
+    email, password = request.form.get('email'), request.form.get('password')
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({"email": email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].get('id'))
+    res = jsonify(user[0].to_json())
+    res.set_cookie(getenv('SESSION_NAME'), session_id)
