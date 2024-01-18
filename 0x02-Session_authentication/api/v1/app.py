7f9e159a921d7ffee1583bdeafda17077e8bf07e
Eugene <eugenemutuyimana15@gmail.com>
2024-01-18 23:45:42 +0200

    added excluded route

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,11 @@ def before_request() -> str:
     if auth is None:
         return
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None\
+            or auth.session_cookie(request) is None:
         abort(401)
     current_user = auth.current_user(request)
     if auth.current_user(request) is None:
