9b828dcfc220a9555cac0a1d433fcdcb25c41744
Eugene <eugenemutuyimana15@gmail.com>
2024-01-18 23:20:29 +0200

    task two

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """creating a new authentication mechanism"""
+    pass
