5a4a267d641a9a38934905939919b521ec33537d
Eugene <eugenemutuyimana15@gmail.com>
2024-01-22 13:27:48 +0200

    update user method implemented

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -7,8 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
-
-from bcrypt import hashpw, gensalt
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -41,21 +40,41 @@ class DB:
         hashed_password -- hashed version of initial password
         Return: returns the user object
         """
-        hashed_password = hashed_password.encode()
-        hashed_version = hashpw(hashed_password, gensalt())
-        user = User(email=email, hashed_password=hashed_version)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
 
-    def find_user_by(self, *args: tuple, **kwargs: dict) -> User:
+    def find_user_by(self, **kwargs: dict) -> User:
         """find user in the db
 
         Keyword arguments:
         args -- arguments with key=value
         Return: returns the first row found in the users table 
         """
+        if not kwargs:
+            raise InvalidRequestError
+
+        col_names = User.__table__.columns.keys()
+        for k in kwargs.keys():
+            if k not in col_names:
+                raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if not user:
             raise NoResultFound
         return user
+
+    def update_user(self, user_id, **kwargs) -> None:
+        """searches for user and update when found"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        col_names = User.__table__.columns.keys()
+        for k in kwargs.keys():
+            if k not in col_names:
+                raise ValueError
+        found_user = self.find_user_by(id=user_id)
+
+        for k, v in kwargs.items():
+            setattr(found_user, k, v)
+        self._session.commit()
