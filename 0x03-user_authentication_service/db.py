ef157e2b8449e720367ad4e4c71fe7a702c672bb
Eugene <eugenemutuyimana15@gmail.com>
2024-01-22 12:17:44 +0200

    find user method added

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+
 from bcrypt import hashpw, gensalt
 from user import Base, User
 
@@ -31,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Save the user to the database
 
         Keyword arguments:
@@ -44,4 +46,16 @@ class DB:
         user = User(email=email, hashed_password=hashed_version)
         self._session.add(user)
         self._session.commit()
+        return user
+
+    def find_user_by(self, *args: tuple, **kwargs: dict) -> User:
+        """find user in the db
+
+        Keyword arguments:
+        args -- arguments with key=value
+        Return: returns the first row found in the users table 
+        """
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
         return user
