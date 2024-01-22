98223bd4acc8727fa2667fb49262c8fa4fdfc899
Eugene <eugenemutuyimana15@gmail.com>
2024-01-22 17:33:40 +0200

    creation of session id implemented

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,14 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """It takes an email string argument and returns the session ID as a string."""
+        try:
+            found_user = self._db.find_user_by(email=email)
+            user_session_id = _generate_uuid()
+            self._db.update_user(
+                found_user.id, session_id=user_session_id)
+            return found_user.session_id
+        except NoResultFound:
+            return None
