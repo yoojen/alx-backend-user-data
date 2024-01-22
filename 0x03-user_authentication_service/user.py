2cdf2421a8e0452d6a7978fb7929ebdc612d1151
Eugene <eugenemutuyimana15@gmail.com>
2024-01-22 11:21:09 +0200

    user model

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""module that defines the user model in the db"""
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Integer, String, Column
+
+Base = declarative_base()
+
+
+class User(Base):
+    """User model definition"""
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(256), nullable=False)
+    hashed_password = Column(String(256), nullable=False)
+    session_id = Column(String(256))
+    reset_token = Column(String(256))
