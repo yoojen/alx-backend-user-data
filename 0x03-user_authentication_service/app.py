9d5ba7c8a70175025dd298effa6ee1059472dbbd
Eugene <eugenemutuyimana15@gmail.com>
2024-01-23 14:41:15 +0200

    last commit 19 task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,5 +84,18 @@ def get_reset_password_token():
     return jsonify({"email": email, "reset_token": reset_token})
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password():
+    """update the password for the user found"""
+    email, reset_token, new_password = request.form.get('email', None), \
+        request.form.get('reset_token', None), \
+        request.form.get('new_password', None)
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000")
