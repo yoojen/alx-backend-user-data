8e6eec99d31200e9d969b3c0a717987a8b030169
Eugene <140322568+yoojen@users.noreply.github.com>
2024-01-23 15:22:18 +0200

    Update auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,8 +40,7 @@ class Auth:
             self._db.find_user_by(email=email)
         except NoResultFound:
             hashed_password = _hash_password(password)
-            if hashed_password:
-                registered_user = self._db.add_user(email, hashed_password)
+            registered_user = self._db.add_user(email, hashed_password)
             return registered_user
         else:
             raise ValueError(f"User {email} already exists")
